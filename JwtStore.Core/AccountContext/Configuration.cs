1ec692173ed145205a4dfa961ba02210f85170f1
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-12 20:08:50 -0300

    Configuração de Password, JWT e ApiKey

---
+++ JwtStore.Core/AccountContext/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.AccountContext
+{
+    public static class Configuration
+    {
+        public static SecretsConfiguration Secrets { get; set; } = new();
+
+        public class SecretsConfiguration
+        {
+            public string ApiKey { get; set; } = string.Empty;
+            public string JwtPrivateKey { get; set; } = string.Empty;
+
+            public string PasswordSaltKey { get; set; } = string.Empty;
+        }
+    }
+}

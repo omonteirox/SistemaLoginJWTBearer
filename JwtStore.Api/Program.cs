26d1af63126c006008e8a6aa2174a3d086b9475c
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-19 00:03:51 -0300

    main

--- JwtStore.Api/Program.cs
+++ JwtStore.Api/Program.cs
@@ -1,6 +1,19 @@
+using JwtStore.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddConfiguration();
+builder.AddDatabase();
+builder.AddJwtAuthentication();
+
+builder.AddAccountContext();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapAccountEndpoints();
 app.Run();

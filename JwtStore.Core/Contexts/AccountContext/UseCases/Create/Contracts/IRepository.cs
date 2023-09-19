44c1c637adb2b537fc2814d81b2600dd7dcb47f7
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-19 00:04:15 -0300

    Icontracts

---
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Contracts/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using JwtStore.Core.Contexts.AccountContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts
+{
+    public interface IRepository
+    {
+        Task<bool> AnyAsync(string email, CancellationToken cancellation);
+
+        Task SaveAsync(User user, CancellationToken cancellation);
+    }
+}
---
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Contracts/IService.cs
@@ -0,0 +1,14 @@
+﻿using JwtStore.Core.Contexts.AccountContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts
+{
+    public interface IService
+    {
+        Task SendEmailAsync(User user, CancellationToken cancellation);
+    }
+}

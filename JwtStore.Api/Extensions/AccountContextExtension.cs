b2997d8808caf82e97d70cde9583dfb96e496605
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-19 00:03:46 -0300

    Extensions methods

---
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace JwtStore.Api.Extensions
+{
+    public static class AccountContextExtension
+    {
+        public static void AddAccountContext(this WebApplicationBuilder builder)
+        {
+            #region Create
+
+            builder.Services.AddTransient<
+                Core.Contexts.AccountContext.UseCases.Create.Contracts.IRepository,
+                Infra.Contexts.AccountContext.UseCases.Create.Repository>();
+
+            builder.Services.AddTransient<
+               Core.Contexts.AccountContext.UseCases.Create.Contracts.IService,
+               Infra.Contexts.AccountContext.UseCases.Create.Service>();
+
+            #endregion Create
+        }
+
+        public static void MapAccountEndpoints(this WebApplication app)
+        {
+            #region Create
+        }
+    }
+}
---
+++ JwtStore.Api/Extensions/BuilderExtension.cs
@@ -0,0 +1,48 @@
+﻿using JwtStore.Core.Contexts.AccountContext;
+using JwtStore.Infra.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JwtStore.Api.Extensions
+{
+    public static class BuilderExtension
+    {
+        public static void AddConfiguration(this WebApplicationBuilder builder)
+        {
+            Configuration.Database.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            Configuration.Secrets.ApiKey = builder.Configuration["Secrets:ApiKey"];
+            Configuration.Secrets.PasswordSaltKey = builder.Configuration["Secrets:PasswordSaltKey"];
+            Configuration.Secrets.JwtPrivateKey = builder.Configuration["Secrets:JwtPrivateKey"];
+        }
+
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            // pode usar migrations
+            builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(
+                Configuration.Database.ConnectionString, b => b.MigrationsAssembly("JwtStore.Api")));
+        }
+
+        public static void AddJwtAuthentication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.Secrets.JwtPrivateKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            }
+                );
+            builder.Services.AddAuthorization();
+        }
+    }
+}

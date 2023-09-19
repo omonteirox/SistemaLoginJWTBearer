72b0f6c3ab125722afcb528db1c89b69ad737eb7
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-19 00:04:37 -0300

    Configuration

---
+++ JwtStore.Core/Contexts/AccountContext/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace JwtStore.Core.Contexts.AccountContext
+{
+    public static class Configuration
+    {
+        public static SecretsConfiguration Secrets { get; set; } = new();
+        public static DatabaseConfiguration Database { get; set; } = new();
+
+        public class DatabaseConfiguration
+        {
+            public string ConnectionString { get; set; } = string.Empty;
+        }
+
+        public class SecretsConfiguration
+        {
+            public string ApiKey { get; set; } = string.Empty;
+            public string JwtPrivateKey { get; set; } = string.Empty;
+
+            public string PasswordSaltKey { get; set; } = string.Empty;
+        }
+    }
+}
